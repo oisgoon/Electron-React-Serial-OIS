39653f4fd205fae456d76bb5fda434d9643a99cb
oisgoon <oinse719@gmail.com>
2022-02-25 02:14:21 +0900

    small additional fixes

--- src/components/ReceivePanel.tsx
+++ src/components/ReceivePanel.tsx
@@ -5,17 +5,11 @@ import Console from 'lib/console';
 
 const ReceivePanel = () => {
   const [receiveData, setReceiveData] = useState<string[]>([]);
-  const foo = useRef(null);
-  const time = new Date();
-  const scrollRef: React.MutableRefObject<any> = useRef();
+  const scrollRef = useRef();
   const [scrollY, setScrollY] = useState<number>(0);
 
   const listener = () => {
     setScrollY(window.pageYOffset);
-  };
-
-  const onChangeScroll = () => {
-    listener();
   };
 
   const onTime = () => {
@@ -36,7 +30,8 @@ const ReceivePanel = () => {
     window.electron.ipcRenderer.receiveOnce('read_data', (data: any) => {
       setReceiveData(receiveData.concat(data));
       return () => {
-        window.removeEventListener('scroll', listener);
+        // window.removeEventListener('scroll', listener);
+        // onChangeScroll();
       };
     });
   }, [receiveData]);
@@ -62,7 +57,7 @@ const ReceivePanel = () => {
         <textarea
           ref={scrollRef}
           value={receiveData.join('')}
-          onChange={onChangeScroll}
+          // onChange={onChangeScroll}
           className="receive_data"
         />
       </fieldset>
